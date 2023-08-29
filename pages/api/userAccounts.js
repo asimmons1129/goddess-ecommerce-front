589b75fedfc0cb3b930686fed7afbd9ee04be62c
Andre Simmons <74680937+asimmons1129@users.noreply.github.com>
2023-08-29 13:27:57 -0500

    Update userAccounts.js

--- pages/api/userAccounts.js
+++ pages/api/userAccounts.js
@@ -3,7 +3,7 @@ import { mongooseConnect } from "../../lib/mongoose";
 
 export default async function handle(req, res){
     await mongooseConnect();
-    /*if(req.method === 'PUT'){
+    if(req.method === 'PUT'){
         const {user} = await getServerSession(req, res, authOptions);
         const userAddress = await UserAccount.findOne({userEmail:user.email});
         if(userAddress){
@@ -16,5 +16,5 @@ export default async function handle(req, res){
         const {user} = await getServerSession(req, res, authOptions);
         const userAddress = await UserAccount.findOne({userEmail:user.email});
         res.json(userAddress);
-    }*/
+    }
 }
